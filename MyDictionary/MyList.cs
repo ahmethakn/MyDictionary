af7d7b8e77e0c4f17b8839a87ecd5f288cf4eea8
Ahmet Hakan <ahmethakanaltunn@hotmail.com>
2021-02-12 11:36:44 +0300

    Add project files.

---
+++ MyDictionary/MyList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyList<A>
+    {
+        A[] units;
+        public MyList()
+        {
+            units = new A[0];
+        }
+        public void Add(A unit)
+        {
+            A[] tempArray = units;
+            units = new A[units.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                units[i] = tempArray[i];
+            }
+            units[units.Length - 1] = unit;
+        }
+        public A[] Units
+        {
+            get { return units; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> unit = new MyList<int>();
+            unit.Add(1);
+            unit.Add(2);
+            foreach (var items in unit.Units)
+            {
+                Console.WriteLine(items);
+            }
+        }
+    }
+}
